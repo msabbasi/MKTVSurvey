fc480ee513bb44fb40086eb753c6cae8b8399e7b
msabbasi <msabbasi@ualberta.ca>
2017-05-12 05:41:20 -0600

    Allowed display of ids for admin

--- survey/admin.py
+++ survey/admin.py
@@ -10,28 +10,29 @@ from .models import Survey, Question, ChoiceGroup, Choice, Submission, Answer
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
-
+    readonly_fields=('id',)
 
 class ChoiceGroupAdmin(admin.ModelAdmin):
-    #fieldsets = [
-    #    (None,               {'fields': ['question_text']}),
-    #    ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    #]
     inlines = [ChoiceInline]
+    readonly_fields=('id',)
 
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 3
+    readonly_fields=('id',)
 
 class SurveyAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+    readonly_fields=('id',)
 
 class AnswerInline(admin.TabularInline):
     model = Answer
     extra = 0
+    readonly_fields=('id',)
 
 class SubmissionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
+    readonly_fields=('id',)
 
 
 admin.site.register(ChoiceGroup, ChoiceGroupAdmin)
